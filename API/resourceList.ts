394fda40de9a19c5165135d7143eb01ed73cb364
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2022-11-26 08:48:55 +0100

    Update filters

--- API/resourceList.ts
+++ API/resourceList.ts
@@ -3,7 +3,7 @@ import { Resources } from '../utils/GeneralProps';
 
 
 export const getResourceList = async ({ tag, search }: { tag: string, search: string }) => {
-  const urlToUse = search !== 'true' ? `/resources?filters[tags][name][$contains]=${tag}&populate=*` : `/resources?filters[$or][0][name][$contains]=${tag}&filters[$or][1][tags][name][$contains]=${tag}&populate=*`
+  const urlToUse = search !== 'true' ? `/resources?filters[tags][name][$containsi]=${tag}&populate=*` : `/resources?filters[$or][0][name][$containsi]=${tag}&filters[$or][1][tags][name][$containsi]=${tag}&populate=*`
 
   const response = await API.get(urlToUse);
 
