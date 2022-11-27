25833bde188dec3cde2ef160f4f4f769789948f0
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2022-11-27 04:50:43 +0100

    Update pagination and refactored

--- API/resourceList.ts
+++ API/resourceList.ts
@@ -3,9 +3,16 @@ import { Resources } from '../utils/GeneralProps';
 
 
 export const getResourceList = async ({ tag, search }: { tag: string, search: string }) => {
-  const urlToUse = search !== 'true' ? `/resources?filters[tags][name][$containsi]=${tag}&populate=*` : `/resources?filters[$or][0][name][$containsi]=${tag}&filters[$or][1][tags][name][$containsi]=${tag}&populate=*`
 
-  const response = await API.get(urlToUse);
+  const pagination = 'pagination[pageSize]=100';
+
+  const tagFilters = `filters[tags][name][$containsi]=${tag}`;
+  const searchFilters = `filters[$or][0][name][$containsi]=${tag}&filters[$or][1][tags][name][$containsi]=${tag}`
+
+
+  const filters = search !== 'true' ? tagFilters : { searchFilters }
+
+  const response = await API.get(`/resources?${filters}&${pagination}&populate=*`);
 
   return response.data.data as Resources[]
 }
