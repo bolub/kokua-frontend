e2f4f5cf2203ccda327bce701ff5e37d262ab587
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2022-12-01 10:21:30 +0100

    Remove top margin and updat etypes

--- components/landing/TagsSection.tsx
+++ components/landing/TagsSection.tsx
@@ -1,36 +1,39 @@
 import { Badge, Skeleton, Wrap, WrapItem } from '@chakra-ui/react';
 import Link from 'next/link';
 import { FC } from 'react';
-import { Tag } from '../../utils/GeneralProps';
+import { TagInner } from '../../utils/GeneralProps';
 
 const TagsSection: FC<{
-  data: Tag[];
-}> = ({ data }) => {
+  data: TagInner[];
+  isLoaded?: boolean;
+}> = ({ data, isLoaded }) => {
   return (
-    <Wrap mt='20px' spacingX='12px' spacingY={'7px'}>
-      {data?.map((t) => (
-        <WrapItem key={t.id}>
-          <Skeleton isLoaded={Boolean(t.attributes.name)}>
-            <Link href={`/resources/${t.attributes.name}`} passHref>
-              <Badge
-                as='a'
-                py={'1'}
-                px={'14px'}
-                borderRadius={'200px'}
-                color={'text.100'}
-                borderColor={'border.200'}
-                borderWidth={'1px'}
-                fontWeight={'600'}
-                textTransform={'capitalize'}
-                fontSize={'12px'}
-                bg='white'
-              >
-                {t.attributes.name}
-              </Badge>
-            </Link>
-          </Skeleton>
-        </WrapItem>
-      ))}
+    <Wrap spacingX='12px' spacingY={'7px'}>
+      {data?.map((t) => {
+        return (
+          <WrapItem key={t.id}>
+            <Skeleton isLoaded={isLoaded}>
+              <Link href={`/resources/${t.attributes.name}`} passHref>
+                <Badge
+                  as='a'
+                  py={'1'}
+                  px={'14px'}
+                  borderRadius={'200px'}
+                  color={'text.100'}
+                  borderColor={'border.200'}
+                  borderWidth={'1px'}
+                  fontWeight={'600'}
+                  textTransform={'capitalize'}
+                  fontSize={'12px'}
+                  bg='white'
+                >
+                  {t.attributes.name}
+                </Badge>
+              </Link>
+            </Skeleton>
+          </WrapItem>
+        );
+      })}
     </Wrap>
   );
 };
