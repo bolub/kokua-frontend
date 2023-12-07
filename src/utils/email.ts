9467dc8eb8fb3be737b0399bda3e6aed52fc1182
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-12-07 15:36:13 +0200

    remove key

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import { CreateEmailOptions } from "resend/build/src/emails/interfaces";
 
-const resend = new Resend("re_HKhM6FUt_F9HZFjq515YpSKohEH5dQ8xF");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (emailDetails: CreateEmailOptions) => {
   return await resend.emails
