744bea39507347f2afed94d7ff694da11c83ef6b
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-12-08 22:35:02 +0200

    Add help text for fullname / alias

--- src/containers/homepage/components/Sidebar/SuggestResource/PersonalDetailsSection.tsx
+++ src/containers/homepage/components/Sidebar/SuggestResource/PersonalDetailsSection.tsx
@@ -2,11 +2,32 @@ import {
   Box,
   Button,
   FormControl,
+  FormHelperText,
   FormLabel,
+  Icon,
   Input,
   Text,
   VStack,
 } from "@chakra-ui/react";
+
+const InfoIcon = (props: any) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+      />
+    </svg>
+  );
+};
 
 export const PersonalDetailsSection = ({
   isVisible,
@@ -29,7 +50,7 @@ export const PersonalDetailsSection = ({
 
         <VStack
           align="start"
-          spacing={isVisible ? 5 : 0}
+          spacing={isVisible ? 8 : 0}
           mt={isVisible ? "16px" : ""}
         >
           <FormControl isRequired hidden={isVisible ? false : true}>
@@ -42,6 +63,13 @@ export const PersonalDetailsSection = ({
               variant="filled"
               placeholder="Chandler bing"
             />
+            <FormHelperText display="flex" alignItems="center" gap="3px">
+              <Icon fontSize="sm" as={InfoIcon} />
+
+              <Text as="span" fontSize="xs">
+                We use this for recognition when the resource is published
+              </Text>
+            </FormHelperText>
           </FormControl>
 
           <FormControl isRequired hidden={isVisible ? false : true}>
