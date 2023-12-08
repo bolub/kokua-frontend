7600eda105400797fe4f78e4b7132cf0ae7373cf
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-12-08 15:32:51 +0200

    test again

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Providers } from "../(website)/providers";
 import { DataLayer } from "@/components/DataLayer";
+
 export const metadata: Metadata = {
   title: "Kokua",
   description:
     "Kokua is a place for developers to explore different resources for their daily tasks",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
