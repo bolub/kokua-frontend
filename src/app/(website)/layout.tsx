2fea473d461ab59ae110782df7712dd8ef97c502
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-12-09 10:32:12 +0200

    Add opengraph image

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -6,6 +6,11 @@ export const metadata: Metadata = {
   title: "Kokua",
   description:
     "Kokua is a place for developers to explore different resources for their daily tasks",
+  openGraph: {
+    images: [
+      "https://res.cloudinary.com/bolub/image/upload/v1702110661/Kokua/KokuaOgImage.png",
+    ],
+  },
 };
 
 export default function RootLayout({
