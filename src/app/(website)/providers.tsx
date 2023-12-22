64b18a3863bf1f0d5f2b5d1e82a864330ca6101c
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-12-22 09:03:11 +0200

    Test custom config for plausible

--- src/app/(website)/providers.tsx
+++ src/app/(website)/providers.tsx
@@ -40,19 +40,54 @@ export function Providers({ children }: { children: React.ReactNode }) {
         }}
       />
 
-      <CacheProvider>
-        <PlausibleProvider domain="kokua.wiki">
-          <ChakraProvider theme={theme}>
-            {children}
+      <Script
+        id="plausible"
+        defer
+        data-domain="kokua.wiki"
+        src="https://plausible.io/js/script.manual.js"
+      />
 
-            <ProgressBar
-              height="4px"
-              color="#0018E7"
-              options={{ showSpinner: false }}
-              shallowRouting
-            />
-          </ChakraProvider>
-        </PlausibleProvider>
+      <Script
+        id="plausible-setup"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }
+        `,
+        }}
+      />
+
+      <Script
+        id="plausible-tag-setup"
+        dangerouslySetInnerHTML={{
+          __html: `
+          function prepareUrl(params) {
+            const url = new URL(location.href)
+            const queryParams = new URLSearchParams(location.search)
+            let customUrl = url.protocol + "//" + url.hostname + url.pathname.replace(/\/$/, '')
+            for (const paramName of params) {
+              const paramValue = queryParams.get(paramName)
+              if (paramValue) customUrl = customUrl + '/' + paramValue
+            }
+            return customUrl
+          }
+          plausible('pageview', { u: prepareUrl(["query", "tag", ... ]) + window.location.search })
+        `,
+        }}
+      />
+
+      <CacheProvider>
+        {/* <PlausibleProvider domain="kokua.wiki"> */}
+        <ChakraProvider theme={theme}>
+          {children}
+
+          <ProgressBar
+            height="4px"
+            color="#0018E7"
+            options={{ showSpinner: false }}
+            shallowRouting
+          />
+        </ChakraProvider>
+        {/* </PlausibleProvider> */}
       </CacheProvider>
     </>
   );
