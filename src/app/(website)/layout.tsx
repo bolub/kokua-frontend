d87c4a72c8bdc1d191520c9a6b32da8dc86ee9a3
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-12-29 10:30:26 +0200

    Add better page titles

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,17 +1,5 @@
-import type { Metadata } from "next";
 import { Providers } from "../(website)/providers";
 import { DataLayer } from "@/components/DataLayer";
-
-export const metadata: Metadata = {
-  title: "Kokua",
-  description:
-    "Kokua is a place for developers to explore different resources for their daily tasks",
-  openGraph: {
-    images: [
-      "https://res.cloudinary.com/bolub/image/upload/v1702110661/Kokua/KokuaOgImage.png",
-    ],
-  },
-};
 
 export default function RootLayout({
   children,
--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,4 +1,5 @@
 import { HomePage } from "@/containers/homepage/HomePage";
+import { Metadata, ResolvingMetadata } from "next";
 
 export type SearchParams = {
   query?: string;
@@ -10,6 +11,36 @@ type HomePageProps = {
 };
 
 export const revalidate = 1;
+
+export async function generateMetadata(
+  {
+    searchParams,
+  }: {
+    searchParams: SearchParams;
+  },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const previousImages = (await parent).openGraph?.images || [];
+
+  const { query, tag } = searchParams;
+
+  const tagTitle = decodeURIComponent(tag || "").replace("&", " | ");
+  const queryTitle = decodeURIComponent(query || "");
+
+  const title = `${queryTitle ? `${queryTitle} | ` : ""}${tagTitle}`;
+
+  return {
+    title: title || "Kokua",
+    description:
+      "Kokua is a place for developers to explore different resources for their daily tasks",
+    openGraph: {
+      images: [
+        "https://res.cloudinary.com/bolub/image/upload/v1702110661/Kokua/KokuaOgImage.png",
+        ...previousImages,
+      ],
+    },
+  };
+}
 
 export default async function Home(props: HomePageProps) {
   return (
