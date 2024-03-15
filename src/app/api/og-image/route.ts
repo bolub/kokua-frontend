755c2b286aecda21780255a722828adde0387b0b
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2024-03-15 19:33:36 +0200

    fix header with env

--- src/app/api/og-image/route.ts
+++ src/app/api/og-image/route.ts
@@ -27,7 +27,10 @@ export async function GET(request: NextRequest) {
       message: "success",
       absoluteOgImageUrl,
       headers: {
-        "Access-Control-Allow-Origin": serverBaseUrl,
+        "Access-Control-Allow-Origin":
+          process.env.NODE_ENV === "production"
+            ? "https://www.kokua.wiki"
+            : serverBaseUrl,
         "Access-Control-Allow-Methods": "GET",
       },
     });
